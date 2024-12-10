a4e52037d1f8ab57cb3d2ea86f46ffb743cedb93
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-10 21:25:23 +0100

    Added Styling Option to use "Inter" as standard Font

--- webshop/tailwind.config.js
+++ webshop/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{html,ts,css}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'ui-sans-serif', 'system-ui']
+            }
+        }
+    },
+    plugins: []
 };
