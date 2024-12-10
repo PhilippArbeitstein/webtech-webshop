785b48f98904c0764370c0b0494b1d68fda4cf99
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-10 23:35:05 +0100

    Small Design change because of nested Containers. Added Centered Title

--- webshop/src/app/landing-page/landing-page.component.ts
+++ webshop/src/app/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
     selector: 'app-landing-page',
-    imports: [NavbarComponent],
+    imports: [NavbarComponent, FooterComponent],
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.component.css'
 })
