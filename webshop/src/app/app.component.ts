0d707bd6302200ef06ef0e507687813af98f76cf
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-10 21:26:32 +0100

    Added Navbar to Main Page, removing the BoilerPlate Code

--- webshop/src/app/app.component.ts
+++ webshop/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+//import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [NavbarComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'webshop';
+    title = 'webshop';
 }
