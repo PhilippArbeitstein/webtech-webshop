c47d5847740e7ca01ec418299839bf42d2fb216a
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-10 20:11:51 +0100

    Created Authentication Service

---
+++ webshop/src/app/auth.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private isAuthenticated = new BehaviorSubject<boolean>(false);
+  isLoggedIn$ = this.isAuthenticated.asObservable();
+
+  constructor(private httpClient: HttpClient) {}
+}
