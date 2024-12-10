3fef7dc07b031e7ce4cf19d9ea3554d82d644c53
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-10 23:44:01 +0100

    Added Login and Register Route with sample Code to show that it works.

--- webshop/src/app/app.routes.ts
+++ webshop/src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [{ path: '', component: LandingPageComponent }];
+export const routes: Routes = [
+    { path: '', component: LandingPageComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent }
+];
