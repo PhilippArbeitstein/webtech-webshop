68a4a0d59cbe087b913856486f9d63f577816579
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-11 14:51:03 +0100

    Added Registration and Login Form with some Validation

--- webshop/src/app/login/login.component.ts
+++ webshop/src/app/login/login.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import {
+    FormBuilder,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-login',
-    imports: [NavbarComponent, FooterComponent],
+    imports: [
+        NavbarComponent,
+        FooterComponent,
+        ReactiveFormsModule,
+        CommonModule
+    ],
     templateUrl: './login.component.html',
     styleUrl: './login.component.css'
 })
-export class LoginComponent {}
+export class LoginComponent {
+    loginForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.loginForm = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required]
+        });
+    }
+
+    onSubmit() {
+        if (this.loginForm.invalid) {
+            this.loginForm.markAllAsTouched();
+        } else {
+            console.log('Form submitted', this.loginForm.value);
+        }
+    }
+}
--- webshop/src/app/register/register.component.ts
+++ webshop/src/app/register/register.component.ts
@@ -1,11 +1,61 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import {
+    FormBuilder,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-register',
-  imports: [],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+    selector: 'app-register',
+    imports: [
+        FooterComponent,
+        NavbarComponent,
+        CommonModule,
+        ReactiveFormsModule
+    ],
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+    currentStep = 1;
+    step1Form: FormGroup;
+    step2Form: FormGroup;
 
+    constructor(fb: FormBuilder) {
+        this.step1Form = fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(8)]]
+        });
+
+        this.step2Form = fb.group({
+            city: ['', Validators.required],
+            address: ['', Validators.required],
+            province: ['', Validators.required]
+        });
+    }
+
+    goToFormStep(step: number) {
+        if (step === 2 && this.step1Form.invalid) {
+            this.markFormGroupTouched(this.step1Form);
+            return;
+        }
+        this.currentStep = step;
+    }
+
+    submitForm() {
+        if (this.step2Form.invalid) {
+            this.markFormGroupTouched(this.step2Form);
+            return;
+        }
+    }
+
+    private markFormGroupTouched(formGroup: FormGroup) {
+        Object.values(formGroup.controls).forEach((control) => {
+            control.markAsTouched();
+        });
+    }
 }
