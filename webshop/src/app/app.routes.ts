8f63d4bc91407d48a610611db9cd0795a071a01a
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-11 22:19:05 +0100

    Added Component and Routes for visiting own products (seller view)

--- webshop/src/app/app.routes.ts
+++ webshop/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './components/login/login.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { MessagePageComponent } from './components/message-page/message-page.component';
 import { ProfilePageComponent } from './components/profile-page/profile-page.component';
+import { OwnProductsPageComponent } from './components/own-products-page/own-products-page.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
     { path: 'messages', component: MessagePageComponent },
-    { path: 'profile', component: ProfilePageComponent }
+    { path: 'profile', component: ProfilePageComponent },
+    { path: 'own-products', component: OwnProductsPageComponent }
 ];
