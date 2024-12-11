2d517d02d45fd65291e990fc0ac1bbb644be0d0d
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-11 14:54:22 +0100

    Moved Components into Components Subfolder and updated imports

--- webshop/src/app/app.routes.ts
+++ webshop/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
