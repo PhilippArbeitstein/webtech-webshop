af855abf36969b805fba524d1e2d75a6058265ea
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-11 21:08:53 +0100

    Implemented: Find User by ID Backend

--- backend/src/user/user.js
+++ backend/src/user/user.js
@@ -4,10 +4,26 @@ const router = express.Router();
 const pool = require('../pool');
 const checkAuth = require('../auth/auth.js');
 
-router.get('/self', checkAuth, (req, res) => {
-    res.status(200).json({
-        message: 'User Routes Work'
-    });
+router.get('/:userId', checkAuth, async (req, res) => {
+    const userId = parseInt(req.params.userId);
+    if (!userId) {
+        return res.status(400).json({ error: 'Invalid user ID.' });
+    }
+
+    try {
+        const query = `SELECT * FROM users WHERE user_id = $1`;
+        const result = await pool.query(query, [userId]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+
+        const user = result.rows[0];
+        res.status(200).json(user);
+    } catch (error) {
+        console.error('Error accessing the database:', error);
+        res.status(500).json({ error: 'Internal server error.' });
+    }
 });
 
 module.exports = router;
