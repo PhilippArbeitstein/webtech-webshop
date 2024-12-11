434d005f7d50463cb5f2f74e491c38b1bc3be9fa
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-11 15:50:03 +0100

    Add Login Backend Call from SingInComponent via Observable in AuthService

--- webshop/src/app/components/login/login.component.ts
+++ webshop/src/app/components/login/login.component.ts
@@ -8,6 +8,7 @@ import {
     Validators
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
     selector: 'app-login',
@@ -23,7 +24,7 @@ import { CommonModule } from '@angular/common';
 export class LoginComponent {
     loginForm: FormGroup;
 
-    constructor(private fb: FormBuilder) {
+    constructor(private fb: FormBuilder, private authService: AuthService) {
         this.loginForm = this.fb.group({
             email: ['', [Validators.required, Validators.email]],
             password: ['', Validators.required]
@@ -34,7 +35,19 @@ export class LoginComponent {
         if (this.loginForm.invalid) {
             this.loginForm.markAllAsTouched();
         } else {
-            console.log('Form submitted', this.loginForm.value);
+            this.authService
+                .login({
+                    email: this.loginForm.get('email')?.value,
+                    password: this.loginForm.get('password')?.value
+                })
+                .subscribe({
+                    next: (response) => {
+                        console.log(response.message);
+                    },
+                    error: (err) => {
+                        console.log('Login failed:', err);
+                    }
+                });
         }
     }
 }
--- webshop/src/app/services/auth.service.ts
+++ webshop/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { response } from 'express';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -10,4 +11,23 @@ export class AuthService {
     isLoggedIn$ = this.isAuthenticated.asObservable();
 
     constructor(private httpClient: HttpClient) {}
+
+    login(loginCredentials: {
+        email: string;
+        password: string;
+    }): Observable<any> {
+        // Pipe is being used to chain RxJS operators. In this case to apply the tap operator to the observable
+        return this.httpClient
+            .post('http://localhost:3000/login', loginCredentials)
+            .pipe(
+                // Tap is a side-effect, that lets you perform side-effects on each emission of the observable without changing the observables data
+                tap((response: any) => {
+                    if (response.message === 'Login successful') {
+                        this.isAuthenticated.next(true);
+                    } else {
+                        this.isAuthenticated.next(false);
+                    }
+                })
+            );
+    }
 }
