e5a6bb630530616358b36925584ec260414edd80
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-12 14:45:46 +0100

    Added Cards for different market places with default navigation to home

--- webshop/src/app/components/landing-page/landing-page.component.ts
+++ webshop/src/app/components/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-landing-page',
-    imports: [NavbarComponent, FooterComponent],
+    imports: [NavbarComponent, FooterComponent, RouterLink],
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.component.css'
 })
