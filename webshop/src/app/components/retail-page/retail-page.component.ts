1819be52adc3c9d7e2b258bf01827b6f2bb098d9
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-12 15:46:16 +0100

    Added Marketplace navigation with conditional rendering of searchbar and footer image

--- webshop/src/app/components/retail-page/retail-page.component.ts
+++ webshop/src/app/components/retail-page/retail-page.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { SearchbarService } from '../../services/searchbar.service';
 
 @Component({
-  selector: 'app-retail-page',
-  imports: [],
-  templateUrl: './retail-page.component.html',
-  styleUrl: './retail-page.component.css'
+    selector: 'app-retail-page',
+    imports: [NavbarComponent, FooterComponent],
+    templateUrl: './retail-page.component.html',
+    styleUrl: './retail-page.component.css'
 })
 export class RetailPageComponent {
+    constructor(private searchbarService: SearchbarService) {}
 
+    ngOnInit(): void {
+        this.searchbarService.setSearchBarContext('retail');
+    }
+
+    ngOnDestroy(): void {
+        this.searchbarService.setSearchBarContext(null);
+    }
 }
