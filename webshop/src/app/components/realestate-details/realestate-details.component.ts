b18adccec8dafbd63f5a931ae46480df78e47f1c
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-18 12:32:36 +0100

    Added basic layouting for details view

--- webshop/src/app/components/realestate-details/realestate-details.component.ts
+++ webshop/src/app/components/realestate-details/realestate-details.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { SearchbarService } from '../../services/searchbar.service';
 
 @Component({
-  selector: 'app-realestate-details',
-  imports: [],
-  templateUrl: './realestate-details.component.html',
-  styleUrl: './realestate-details.component.css'
+    selector: 'app-realestate-details',
+    imports: [NavbarComponent, FooterComponent],
+    templateUrl: './realestate-details.component.html',
+    styleUrl: './realestate-details.component.css'
 })
 export class RealestateDetailsComponent {
+    constructor(private searchbarService: SearchbarService) {}
 
+    ngOnInit(): void {
+        this.searchbarService.setSearchBarContext('real-estate');
+    }
+
+    ngOnDestroy(): void {
+        this.searchbarService.setSearchBarContext(null);
+    }
 }
