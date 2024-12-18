53a322381434c4a4dfb2757f72b6934dbbe425ca
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-18 12:28:17 +0100

    Added Route per Card to visit details page and removed wrongly named component

---
+++ webshop/src/app/components/realestate-details/realestate-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-realestate-details',
+  imports: [],
+  templateUrl: './realestate-details.component.html',
+  styleUrl: './realestate-details.component.css'
+})
+export class RealestateDetailsComponent {
+
+}
