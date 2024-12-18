89b22745be34187b1d7290bd9818e6ff12965f70
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-12-18 12:22:34 +0100

    Generated new Component in order to display Details of one List Item

---
+++ webshop/src/app/components/retail-details/retail-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-retail-details',
+  imports: [],
+  templateUrl: './retail-details.component.html',
+  styleUrl: './retail-details.component.css'
+})
+export class RetailDetailsComponent {
+
+}
